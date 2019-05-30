4502cd10f495d2eac895d45a1e757409c8dc01eb
Alex <hello@alex-kielwein.com>
2019-05-31 01:07:19 +0200

    Initial upload

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Audio;
+using UnityEngine;
+using UnityEngine.Audio;
+#pragma warning disable
+
+
+
+ /*@@@@ @@@@@@@  @@@@@@  @@@@@@@   @@@@@@ @@@@@@@@ @@@@@@@@ @@@@@@@ 
+!@@       @@!   @@!  @@@ @@!  @@@ !@@     @@!      @@!      @@!  @@@
+ !@@!!    @!!   @!@!@!@! @!@!!@!   !@@!!  @!!!:!   @!!!:!   @!@  !@!
+    !:!   !!:   !!:  !!! !!: :!!      !:! !!:      !!:      !!:  !!!
+::.: :     :     :   : :  :   : : ::.: :  : :: ::: : :: ::: :: :  : 
+             _                           
+            / | _  __´/_  /_ /_  _ _  _   _ _  _    ._/_
+           /_.'/_// //   /_///_|/ / //_' / / //_', / /                                
+            _  _/_ _  _  _   _ _      _ _  _  _ /_ ._  _ 
+       |/|//_///\_\  /_// / / / //_/ / / //_|/_ / /// //_'
+                                 _/                                
+                                                                   
+.. ..: :.: :.: :.: :.: www.alex.kielwein.com :.: :.: :.: :.: :.. */
+
+
+
+public class AudioManager : MonoBehaviour
+{
+    
+    public Sound[] soundArray; //Sounds Class greift auf dieses Array zu
+    public static AudioManager ManagerInstance; //Prüft ob es nur 1 AM gibt. Public weil shared von allen Instances dieser Class!
+    private bool _activated;
+    private bool _manipulated;
+    
+    private float _manipulatedPitch;
+    private float _newPitch;
+    private float _manipulatedBPM;
+    private float _newPitchShifter;
+    private float _manipulatedLowPass;
+    private float _newLowPass;
+    
+    private const float _standardPitch = 1f;
+    private const float _standardPitchShifter = 1f;
+    private const float _standardLowPass = 5000f;
+
+
+
+    private void Awake()
+    {
+
+        // falls es mehr als 1 AM gibt, destroy das GO auf dem die AM Class liegt
+        if (ManagerInstance == null)
+        {
+            ManagerInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        DontDestroyOnLoad(gameObject); //damit wird der AudioManager nicht neugestartet beim Scene-Wechsel (Hauptmenü => Spiel)
+        
+
+
+        //s.audioSource ist die var aus der AM class, s. aus der Sound Class
+        foreach (var s in soundArray)
+        {
+            s.audioSource = gameObject.AddComponent<AudioSource>();
+            s.audioSource.outputAudioMixerGroup = s.AudioMixerGroup;
+            s.audioSource.clip = s.soundFile;                               
+            s.audioSource.volume = s.startVolume;
+            s.audioSource.loop = s.loop;
+            _manipulatedPitch = s.manipulatedPitch;
+            _manipulatedBPM = s.manipulatedBPM;
+            _manipulatedLowPass = s.manipulatedLowPass;
+        }
+    }
+
+    
+    
+    private void Start()
+    {
+        Play("drum");
+    }
+
+
+    
+    private void Update()
+    {
+        
+        // Inputs:
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _activated = !_activated;
+        }    
+        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            _manipulated = !_manipulated;
+        }  
+        
+        
+        // Sound-Manipulation Calculations:
+        _newPitch = _standardPitch * _manipulatedBPM;
+        _newPitchShifter = _standardPitchShifter / _manipulatedBPM;
+        
+        
+        
+        // When Sound is Activated:
+        if (_activated && !_manipulated)
+        {
+            foreach (var s in soundArray)
+            {
+                s.audioSource.volume = s.activatedVolume;
+                s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("PitchMaster", _standardPitch);
+                s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("PitchShifter", _standardPitchShifter);
+                s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("LowPassMaster", _standardLowPass);
+            }
+        }
+
+        // When Sounds are Manipulated:
+        else if (_activated && _manipulated)
+        { 
+            foreach (var s in soundArray)
+            {
+                if (s.manipulateVolume)
+                {
+                    s.audioSource.volume = s.manipulatedVolume;
+                }
+
+                if (s.manipulateBPM)
+                {
+                    s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("PitchMaster", _newPitch);
+                    s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("PitchShifter", _newPitchShifter);
+                }
+                
+                if (s.manipulatePitch)
+                {
+                    s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("PitchMaster", _manipulatedPitch);
+                }
+                
+                if (s.manipulateLowPass)
+                {
+                    s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("LowPassMaster", _manipulatedLowPass);
+                }
+            }
+        }
+            
+        // When Game starts:
+        else
+        {
+            foreach (var s in soundArray)
+            {
+                s.audioSource.volume = s.startVolume;
+                s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("PitchMaster", _standardPitch);
+                s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("PitchShifter", _standardPitchShifter);
+                s.audioSource.outputAudioMixerGroup.audioMixer.SetFloat("LowPassMaster", _standardLowPass);
+            }
+        }
+        
+    }
+
+    
+    
+    // Play Method mit Debugging
+    public void Play(string name)
+    {
+        var s = Array.Find(soundArray,soundMatch => soundMatch.soundName == name);
+
+        if (s == null)
+        {
+            Debug.LogError("Sound: " + name + " nicht gefunden!");
+            return;
+        }
+
+        s.audioSource.Play();
+    }
+
+    
+    
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+
+ /*@@@@ @@@@@@@  @@@@@@  @@@@@@@   @@@@@@ @@@@@@@@ @@@@@@@@ @@@@@@@ 
+!@@       @@!   @@!  @@@ @@!  @@@ !@@     @@!      @@!      @@!  @@@
+ !@@!!    @!!   @!@!@!@! @!@!!@!   !@@!!  @!!!:!   @!!!:!   @!@  !@!
+    !:!   !!:   !!:  !!! !!: :!!      !:! !!:      !!:      !!:  !!!
+::.: :     :     :   : :  :   : : ::.: :  : :: ::: : :: ::: :: :  : 
+             _                           
+            / | _  __´/_  /_ /_  _ _  _   _ _  _    ._/_
+           /_.'/_// //   /_///_|/ / //_' / / //_', / /                                
+            _  _/_ _  _  _   _ _      _ _  _  _ /_ ._  _ 
+       |/|//_///\_\  /_// / / / //_/ / / //_|/_ / /// //_'
+                                 _/                                
+                                                                   
+.. ..: :.: :.: :.: :.: www.alex.kielwein.com :.: :.: :.: :.: :.. */
+
+
+
+[System.Serializable] // Hierdurch wird die Sounds Class serialized
+public class Sound
+{
+    [Header("Basic Loop Settings:")]
+    [Tooltip("Dieser Name kann ganz einfach per Play('Name') in der Update Method gecalled werden und wird dann abgespielt.")]
+    public string soundName;
+    [Tooltip("Hier muss immer die 'Master' Group einzeln angegen werden, nicht der komplette AudioMixer.")]
+    public AudioMixerGroup AudioMixerGroup;
+    [Tooltip("Welches Sound-File soll abgespielt werden? Es funktionieren WAV und MP3 Dateien.")]
+    public AudioClip soundFile;
+    [Tooltip("Soll der Sound permanent loopen?")]
+    public bool loop = true;
+    [HideInInspector] public AudioSource audioSource;
+    
+    
+    [Header("When Game Starts:")]
+    [Space (10)]
+    [Tooltip("Start-Lautstärke Standard ist: 0")]
+    [Range(0f, 1f)] public float startVolume;
+
+    
+
+    [Header("When Sound Is Activated:")]
+    [Space (10)]
+    [Tooltip("Activated-Lautstärke Standard ist: 1")]
+    [Range(0f, 1f)] public float activatedVolume;
+
+    
+    
+    [Header("When Sounds Are Manipulated:")]
+    [Space (10)]
+    [Tooltip("Soll die Lautstärke manipuliert werden?")]
+    public bool manipulateVolume;
+    [Tooltip("Lautstärke Standard ist: 1")]
+    [Range(0f, 1f)] public float manipulatedVolume;
+    
+    [Space(5)]
+    
+    [Tooltip("Sollen die BPM's manipuliert werden?")]
+    public bool manipulateBPM;
+    [Tooltip("BPM Standard für 100% ist: 1")]
+    [Range(-1f, 3f)] public float manipulatedBPM;
+    
+    [Space(5)]
+    
+    [Tooltip("Soll der Pitch manipuliert werden?")]
+    public bool manipulatePitch;
+    [Tooltip("Pitch Standard ist: 1")]
+    [Range(0.1f, 10f)] public float manipulatedPitch;
+    
+    [Space(5)]
+    
+    [Tooltip("Soll der LowPass manipuliert werden?")]
+    public bool manipulateLowPass;
+    [Tooltip("LowPass Standard ist: 5000")]
+    [Range(500f, 10000f)] public float manipulatedLowPass;
+    
+    
+    
+}
